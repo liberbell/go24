4d839c7e466c74884edaef85cf3c0183017f5873
Liberbell <liberbell@gmail.com>
2022-12-03 14:36:33 +0900

    main3

--- learn1/main.go
+++ learn1/main.go
@@ -6,6 +6,11 @@ func main() {
 	fmt.Println("hello")
 
 	var whatToSay string
+	var i int
+
 	whatToSay = "Goodbye, cruel world."
 	fmt.Println(whatToSay)
+
+	i = 7
+	fmt.Println("i is set to ", i)
 }
