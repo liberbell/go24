baa9f98b088be5a0005818b4c4ee661ed7af11ef
Liberbell <liberbell@gmail.com>
2022-12-04 15:19:38 +0900

    main7

--- learn3/main.go
+++ learn3/main.go
@@ -1,18 +1,29 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
 var s = "seven"
 
+var firstName string
+var lastName string
+var phoneNumber string
+var age int
+var birthday time.Time
+
 func main() {
 	var s2 = "six"
+	// s := "eight"
+
 	log.Println("s is ", s)
 	log.Println("s2 is ", s2)
 
 	saySomething("xxx")
 }
 
-func saySomething(s string) (string, string) {
+func saySomething(s3 string) (string, string) {
 	log.Println("s say from the saySomething func is ", s)
-	return s, "world"
+	return s3, "world"
 }
