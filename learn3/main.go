b5f86bccde5cbf4bb1829717cfb5a365a72f002e
Liberbell <liberbell@gmail.com>
2022-12-04 15:07:29 +0900

    main6

--- learn3/main.go
+++ learn3/main.go
@@ -8,8 +8,11 @@ func main() {
 	var s2 = "six"
 	log.Println("s is ", s)
 	log.Println("s2 is ", s2)
+
+	saySomething("xxx")
 }
 
 func saySomething(s string) (string, string) {
+	log.Println("s say from the saySomething func is ", s)
 	return s, "world"
 }
