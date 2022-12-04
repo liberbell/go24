f9ba3ca5505b09375fa3047296909100b749ae21
Liberbell <liberbell@gmail.com>
2022-12-05 08:31:58 +0900

    main4

--- learn5/main.go
+++ learn5/main.go
@@ -5,11 +5,19 @@ import "log"
 func main() {
 	var isTrue bool
 
-	isTrue = true
+	isTrue = false
 
 	if isTrue == true {
 		log.Println("isTrue is ", isTrue)
 	} else {
 		log.Println("isTrue is ", isTrue)
+	}
+
+	cat := "cat"
+
+	if cat == "cat" {
+		log.Println("Cat is cat")
+	} else {
+		log.Println("Cat is not cat")
 	}
 }
