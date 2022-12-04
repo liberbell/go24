8e2d304fa5bed86d484d2c8363685a950319bcf7
Liberbell <liberbell@gmail.com>
2022-12-05 08:55:10 +0900

    main3

--- learn6/main.go
+++ learn6/main.go
@@ -1,9 +1,17 @@
 package main
 
+import (
+	"log"
+)
+
 func main() {
 	// for i := 0; i <= 5; i++ {
 	// 	log.Println(i)
 	// }
 
 	animals := []string{"dog", "fish", "horse", "cow", "cat"}
+
+	for i, animal := range animals {
+		log.Println(i, animal)
+	}
 }
