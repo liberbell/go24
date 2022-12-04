06c3a43681fd0fefb0946906efd029feb2cb2de0
Liberbell <liberbell@gmail.com>
2022-12-05 08:45:54 +0900

    main8

--- learn5/main.go
+++ learn5/main.go
@@ -21,16 +21,25 @@ func main() {
 		log.Println("Cat is not cat")
 	}
 
-	myNum := 100
-	isTrue := false
+	// myNum := 100
+	// isTrue := false
 
-	if myNum > 99 && !isTrue {
-		log.Println("myNum is greater than 99 and isTrue is set to True")
-	} else if myNum < 100 && isTrue {
-		log.Println("1")
-	} else if myNum == 101 && isTrue {
-		log.Println("2")
-	} else if myNum > 1000 && isTrue == false {
-		log.Println("3")
+	// if myNum > 99 && !isTrue {
+	// 	log.Println("myNum is greater than 99 and isTrue is set to True")
+	// } else if myNum < 100 && isTrue {
+	// 	log.Println("1")
+	// } else if myNum == 101 && isTrue {
+	// 	log.Println("2")
+	// } else if myNum > 1000 && isTrue == false {
+	// 	log.Println("3")
+	// }
+
+	myVar := "cat"
+
+	switch myVar {
+	case "cat":
+		log.Println("Cat is set to cat")
+	case "dog":
+		log.Println("Cat is set to dog")
 	}
 }
