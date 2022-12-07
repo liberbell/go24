d09cfbb8b609dbc67ca2fad3ea146558a04be9c8
Liberbell <liberbell@gmail.com>
2022-12-08 08:43:07 +0900

    main3

--- learn9/main.go
+++ learn9/main.go
@@ -1,11 +1,10 @@
 package main
 
-import "log"
-
 func main() {
-	PrintText("Hi")
+	// PrintText("Hi")
+	intChan := make(chan int)
 }
 
-func PrintText(s string) {
-	log.Println(s)
-}
+// func PrintText(s string) {
+// 	log.Println(s)
+// }
