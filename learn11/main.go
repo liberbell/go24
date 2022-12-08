0d9b98804bf7a344bb96d4f8f6ce8781d258925a
Liberbell <liberbell@gmail.com>
2022-12-09 08:48:16 +0900

    main4

--- learn11/main.go
+++ learn11/main.go
@@ -1,11 +1,17 @@
 package main
 
+import "errors"
+
 func main() {
 	a
 }
 
 func divide(x, y float32) (float32, error) {
 	var result float32
+
+	if y == 0 {
+		return result, errors.New("Cannot divide by 0")
+	}
 	result = x / y
-	return result, 
+	return result, nil
 }
