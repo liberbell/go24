fe9ed56d89920d09a0343d1dda8e3815a6e20bf0
Liberbell <liberbell@gmail.com>
2022-12-09 08:28:56 +0900

    main4

--- learn10/main.go
+++ learn10/main.go
@@ -4,7 +4,7 @@ type Person struct {
 	FirstName string `json:"first_name"`
 	LastName string `json:"last_name"`
 	HairColor string `json:"hair_color"`
-	
+	HasDog bool `json:"has_dog"`
 }
 
 func main() {
