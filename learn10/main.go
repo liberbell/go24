119e43f1b523c2dda719627a8eac1e13944caef5
Liberbell <liberbell@gmail.com>
2022-12-09 08:43:27 +0900

    main12

--- learn10/main.go
+++ learn10/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"strings"
 )
 
 type Person struct {
@@ -60,5 +59,5 @@ func main() {
 	if err != nil {
 		log.Println("error marshalling", err)
 	}
-	fmt.Println(string(newJson)
+	fmt.Println(string(newJson))
 }
