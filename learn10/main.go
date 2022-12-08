ae2395e39176aba880c5ff4030a990724847c449
Liberbell <liberbell@gmail.com>
2022-12-09 08:24:48 +0900

    main1

--- learn10/main.go
+++ learn10/main.go
@@ -8,5 +8,12 @@ func main() {
 			"first_name": "Eric,
 			"last_name": "Crapton,
 			"hair_color": "grya",
-			`
-}
+			"has_dog": true
+		},
+		{
+			"first_name": "Alex",
+			"last_name": "hepp",
+			"hair_color": "black",
+			"has_dog": false
+		}
+	]`
