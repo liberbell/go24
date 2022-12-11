372bf5361fc72987c28a97c8cedb7998f3790738
Liberbell <liberbell@gmail.com>
2022-12-11 11:06:55 +0900

    main9

--- learn12/main.go
+++ learn12/main.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Fprintf(w, "")
 }
 
 func main() {
 	// fmt.Println("Hello World")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello World")
-		if err != nil {
-			log.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	n, err := fmt.Fprintf(w, "Hello World")
+	// 	if err != nil {
+	// 		log.Println(err)
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	// })
+
+	http.HandleFunc("/", Home)
 
 	_ = http.ListenAndServe(":8080", nil)
 }
