b4f5372f4118eb8d5f6290f1fbece4c7ad173113
Liberbell <liberbell@gmail.com>
2022-12-12 08:51:53 +0900

    main17

--- learn12/main.go
+++ learn12/main.go
@@ -43,15 +43,6 @@ func divideValues(x, y float32) (float32, error) {
 }
 
 func main() {
-	// fmt.Println("Hello World")
-
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	n, err := fmt.Fprintf(w, "Hello World")
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 	}
-	// 	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	// })
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
