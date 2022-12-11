eac9bf363d6ec166fe40b2855ed938487681e561
Liberbell <liberbell@gmail.com>
2022-12-12 08:39:28 +0900

    main10

--- learn12/main.go
+++ learn12/main.go
@@ -36,5 +36,7 @@ func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
 	_ = http.ListenAndServe(portNumber, nil)
 }
