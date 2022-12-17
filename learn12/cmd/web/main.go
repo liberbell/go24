03e30e9368b7ba13c56e65c00c0b056af49ccce6
Liberbell <liberbell@gmail.com>
2022-12-17 13:37:27 +0900

    main1

--- learn12/cmd/web/main.go
+++ learn12/cmd/web/main.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/tsawler/go-course/pkg/config"
 	"github.com/tsawler/go-course/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 func main() {
+	var app config.AppConfig
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
