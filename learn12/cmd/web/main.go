4e05e975b53f30a96c9ef60e8fb017ab03c3b9a1
Liberbell <liberbell@gmail.com>
2022-12-17 13:39:27 +0900

    main2

--- learn12/cmd/web/main.go
+++ learn12/cmd/web/main.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/tsawler/go-course/pkg/config"
 	"github.com/tsawler/go-course/pkg/handlers"
+	_ "github.com/tsawler/go-course/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
 	var app config.AppConfig
+
+	tc, err := render.createTemplateCache()
 
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
