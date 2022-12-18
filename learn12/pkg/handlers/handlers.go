c5f22d3f956bcd1b63b6d0017e9441d095494e08
Liberbell <liberbell@gmail.com>
2022-12-18 14:28:29 +0900

    handlers1

--- learn12/pkg/handlers/handlers.go
+++ learn12/pkg/handlers/handlers.go
@@ -3,8 +3,15 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/tsawler/go-course/pkg/config"
 	"github.com/tsawler/go-course/pkg/render"
 )
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
