fa108b5793df62ec419768add9d93c023f360bd6
Liberbell <liberbell@gmail.com>
2022-12-18 14:30:27 +0900

    handlers2

--- learn12/pkg/handlers/handlers.go
+++ learn12/pkg/handlers/handlers.go
@@ -13,6 +13,15 @@ type Repository struct {
 	App *config.AppConfig
 }
 
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+func NewHandlers() {
+
+}
+
 func Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl")
 }
