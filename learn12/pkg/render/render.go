4b1de9d8f19d86820f9fac5f76d895f7edae2181
Liberbell <liberbell@gmail.com>
2022-12-18 14:47:16 +0900

    render7

--- learn12/pkg/render/render.go
+++ learn12/pkg/render/render.go
@@ -19,8 +19,13 @@ func NewTemplates(a *config.AppConfig) {
 }
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
+	var tc map[string]*template.Template
 
-	tc := app.TemplateCache
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc = CreateTemplateCache()
+	}
 
 	//get request template from cache
 	t, ok := tc[tmpl]
