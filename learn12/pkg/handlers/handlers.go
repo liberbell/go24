b433a75078b6a8a31d16260a80b04e4518e28d46
Liberbell <liberbell@gmail.com>
2022-12-19 22:15:28 +0900

    handlers2

--- learn12/pkg/handlers/handlers.go
+++ learn12/pkg/handlers/handlers.go
@@ -12,6 +12,10 @@ type TemplateData struct {
 	IntMap    map[string]int
 	FloatMap  map[string]float32
 	Data      map[string]interface{}
+	CSRFToken string
+	Flash     string
+	Warning   string
+	Error     string
 }
 
 var Repo *Repository
