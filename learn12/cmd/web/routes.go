4b317b91a738ade6542026a6f9e700d20a03d6b2
Liberbell <liberbell@gmail.com>
2022-12-20 08:34:59 +0900

    routes4

--- learn12/cmd/web/routes.go
+++ learn12/cmd/web/routes.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/bmizerany/pat"
 	"github.com/tsawler/go-course/pkg/config"
+	"github.com/tsawler/go-course/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
-	mux.Get("/", http.HandleFunc())
+	mux.Get("/", http.HandleFunc(handlers.Repo.Home))
+	mux.Get("/about", http.HandleFunc(handlers.Repo.About))
 
 	return mux
 }
