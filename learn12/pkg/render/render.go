f0ea2d16630126015d1dc2d71b9570bcafe7ba93
Liberbell <liberbell@gmail.com>
2022-12-19 22:31:47 +0900

    render5

--- learn12/pkg/render/render.go
+++ learn12/pkg/render/render.go
@@ -8,7 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/tsawler/go-course/pkg/config"
-	"github.com/tsawler/go-course/pkg/handlers"
+	"github.com/tsawler/go-course/pkg/models"
 )
 
 var functions = template.FuncMap{}
@@ -19,7 +19,7 @@ func NewTemplates(a *config.AppConfig) {
 	app = a
 }
 
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *handlers.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
 	var tc map[string]*template.Template
 
 	if app.UseCache {
