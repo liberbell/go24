6e53da69c78a50348937c374be4592744840addb
Liberbell <liberbell@gmail.com>
2022-12-20 08:29:41 +0900

    routes5

--- learn12/cmd/web/routes.go
+++ learn12/cmd/web/routes.go
@@ -3,9 +3,13 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/tsawler/go-course/pkg/config"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := pat.New()
+	mux.Get("/", http.HandleFunc())
+
+	return mux
 }
