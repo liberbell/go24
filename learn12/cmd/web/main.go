383edfbc527089b03f70cf05c84504081fdf5208
Liberbell <liberbell@gmail.com>
2022-12-22 08:38:17 +0900

    main2

--- learn12/cmd/web/main.go
+++ learn12/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/tsawler/go-course/pkg/config"
@@ -17,6 +18,7 @@ func main() {
 	var app config.AppConfig
 
 	session := scs.New()
+	session.Lifetime = 24 * time.Hour
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
