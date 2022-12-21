18833e78650e33c5e0af4412e174ec12f9bf7023
Liberbell <liberbell@gmail.com>
2022-12-21 21:39:53 +0900

    routes3

--- learn12/cmd/web/routes.go
+++ learn12/cmd/web/routes.go
@@ -14,8 +14,8 @@ func routes(app *config.AppConfig) http.Handler {
 	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
 	mux := chi.NewRouter()
-	mux.Get("/", handlers.Repo.Home())
-	mux.Get("/about", handlers.Repo.About())
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
