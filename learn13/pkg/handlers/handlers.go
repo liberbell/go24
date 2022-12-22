8a9656a6554957f36099cbd184dbdb23b8c429af
Liberbell <liberbell@gmail.com>
2022-12-23 08:56:29 +0900

    learn13

---
+++ learn13/pkg/handlers/handlers.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/tsawler/go-course/pkg/config"
+	"github.com/tsawler/go-course/pkg/models"
+	"github.com/tsawler/go-course/pkg/render"
+)
+
+var Repo *Repository
+
+type Repository struct {
+	App *config.AppConfig
+}
+
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	StringMap := make(map[string]string)
+	StringMap["test"] = "Hello, again."
+
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	// m.App.Session
+	StringMap["remote_ip"] = remoteIP
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: StringMap,
+	})
+}
---
+++ learn13/pkg/render/render.go
@@ -0,0 +1,123 @@
+package render
+
+import (
+	"bytes"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+
+	"github.com/tsawler/go-course/pkg/config"
+	"github.com/tsawler/go-course/pkg/models"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	//get request template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template from template cache")
+	}
+	buf := new(bytes.Buffer)
+	td = AddDefaultData(td)
+
+	_ = t.Execute(buf, td)
+
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+
+	//render the template
+	// parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
+	// err := parsedTemplate.Execute(w, nil)
+	// if err != nil {
+	// 	fmt.Println("error parsing template:", err)
+	// }
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	//get all of the files name *.page.tmpl from ./templates
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+	// range through the all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
+
+// var tc = make(map[string]*template.Template)
+
+// func RenderTemplate(w http.ResponseWriter, t string) {
+// 	var tmpl *template.Template
+// 	var err error
+
+// 	_, inMap := tc[t]
+// 	if !inMap {
+// 		// log.Println("using cached template")
+// 		log.Println("creating template and adding to cache")
+// 		err = createTemplateCache(t)
+// 		if err != nil {
+// 			log.Println(err)
+// 		}
+// 	} else {
+// 		log.Println("using cached template")
+// 	}
+
+// 	tmpl = tc[t]
+
+// 	err = tmpl.Execute(w, nil)
+// }
+
+// func createTemplateCache(t string) error {
+// 	templates := []string{
+// 		fmt.Sprintf("./templates/%s", t), "./templates/base.layout.tmpl",
+// 	}
+// 	tmpl, err := template.ParseFiles(templates...)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	tc[t] = tmpl
+// 	return nil
+// }
