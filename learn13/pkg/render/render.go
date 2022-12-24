0f914b906523ce8862a4d5810a47b9a7811e16ee
Liberbell <liberbell@gmail.com>
2022-12-24 10:20:29 +0900

    render1

--- learn13/pkg/render/render.go
+++ learn13/pkg/render/render.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"path/filepath"
 
-	"github.com/tsawler/go-course/pkg/config"
-	"github.com/tsawler/go-course/pkg/models"
+	"github.com/tsawler/bookings/pkg/config"
+	"github.com/tsawler/bookings/pkg/models"
 )
 
 var functions = template.FuncMap{}
@@ -85,39 +85,3 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	}
 	return myCache, nil
 }
-
-// var tc = make(map[string]*template.Template)
-
-// func RenderTemplate(w http.ResponseWriter, t string) {
-// 	var tmpl *template.Template
-// 	var err error
-
-// 	_, inMap := tc[t]
-// 	if !inMap {
-// 		// log.Println("using cached template")
-// 		log.Println("creating template and adding to cache")
-// 		err = createTemplateCache(t)
-// 		if err != nil {
-// 			log.Println(err)
-// 		}
-// 	} else {
-// 		log.Println("using cached template")
-// 	}
-
-// 	tmpl = tc[t]
-
-// 	err = tmpl.Execute(w, nil)
-// }
-
-// func createTemplateCache(t string) error {
-// 	templates := []string{
-// 		fmt.Sprintf("./templates/%s", t), "./templates/base.layout.tmpl",
-// 	}
-// 	tmpl, err := template.ParseFiles(templates...)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	tc[t] = tmpl
-// 	return nil
-// }
