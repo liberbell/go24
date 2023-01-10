fd1a20eb22d45f15c8c9c1c4ccd8d7539cac50ae
Liberbell <liberbell@gmail.com>
2023-01-11 08:57:46 +0900

    render1

--- learn13/pkg/render/render.go
+++ learn13/pkg/render/render.go
@@ -59,7 +59,7 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 	// range through the all files ending with *.page.tmpl
 	for _, page := range pages {
 		name := filepath.Base(page)
-		ts, err := template.New(name).ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return myCache, err
 		}
