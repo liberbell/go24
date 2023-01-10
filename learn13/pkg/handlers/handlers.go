01d125091b4059cbd57da94956836860ca2bb16f
Liberbell <liberbell@gmail.com>
2023-01-11 08:48:54 +0900

    handlers4

--- learn13/pkg/handlers/handlers.go
+++ learn13/pkg/handlers/handlers.go
@@ -57,3 +57,7 @@ func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
 }
+
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
+}
