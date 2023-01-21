9e39160296ad766ff9b02a8f4068cd3a73f59cb3
Liberbell <liberbell@gmail.com>
2023-01-21 10:58:00 +0900

    forms4

--- learn13/internal/forms/forms.go
+++ learn13/internal/forms/forms.go
@@ -24,7 +24,7 @@ func New(data url.Values) *Form {
 func (f *Form) Has(field string, r http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.A(field, "This field cannot be blank.")
+		f.Errors.Add(field, "This field cannot be blank.")
 		return false
 	}
 	return true
