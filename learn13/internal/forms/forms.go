40cb079d99cdcdc89d540e9863f6ab6e70ce0756
Liberbell <liberbell@gmail.com>
2023-01-23 08:39:04 +0900

    forms4

--- learn13/internal/forms/forms.go
+++ learn13/internal/forms/forms.go
@@ -1,6 +1,7 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 	"strings"
@@ -42,4 +43,7 @@ func (f *Form) Has(field string, r *http.Request) bool {
 
 func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d character long.", length))
+	}
 }
