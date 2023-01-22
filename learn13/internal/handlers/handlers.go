7f51711c76a0e421bb84001d02209dc08f24517d
Liberbell <liberbell@gmail.com>
2023-01-23 08:41:08 +0900

    handlers3

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -74,6 +74,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	// form.Has("first_name", r)
 	form.Required("first_name", "last_name", "email", "phone")
+	form.MinLength("first_name", 3, r)
 
 	if !form.Valid() {
 		data := make(map[string]interface{})
