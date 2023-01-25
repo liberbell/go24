2ff717905c94e831504b8dc6e1479b110c0163c9
Liberbell <liberbell@gmail.com>
2023-01-26 08:43:54 +0900

    handlers2

--- learn15/websockets-course/internal/handlers.go
+++ learn15/websockets-course/internal/handlers.go
@@ -1,8 +1,16 @@
 package handlers
 
-import "github.com/CloudyKit/jet"
+import (
+	"net/http"
+
+	"github.com/CloudyKit/jet"
+)
 
 var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode(),
 )
+
+func Home(w http.ResponseWriter, r *http.Request)  {
+	data := 	
+}
