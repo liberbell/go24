4642cb138a83b3fd4e8f0e4107cb1e85839a28ee
Liberbell <liberbell@gmail.com>
2023-01-28 15:24:28 +0900

    setup_test7

--- learn13/cmd/web/setup_test.go
+++ learn13/cmd/web/setup_test.go
@@ -11,7 +11,7 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-type myhandler struct{}
+type myHandler struct{}
 
 func (mh *myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
