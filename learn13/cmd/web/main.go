109c89701bee21c56c63f84f9b6964861ba4a60c
Liberbell <liberbell@gmail.com>
2023-01-29 13:04:26 +0900

    main1

--- learn13/cmd/web/main.go
+++ learn13/cmd/web/main.go
@@ -66,6 +66,29 @@ func main() {
 }
 
 func run() error {
+	gob.Register(models.Reservation{})
+	app.InProduction = false
 
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		// return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
 	return nil
+
 }
