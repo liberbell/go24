fdce82d7803e053590469da3e8b9ecd1c74ad8dd
Liberbell <liberbell@gmail.com>
2023-01-29 12:46:09 +0900

    routes2

--- learn13/cmd/web/routes_test.go
+++ learn13/cmd/web/routes_test.go
@@ -6,4 +6,11 @@ func testRoutes(t *testing.T) {
 	var app config.AppConfig
 
 	mux := routes(&app)
+
+	switch v := mux.(type) {
+	case *chi.Mux:
+	default:
+		t.Error(fmt.Sprintf("type is not *chi.Mux: %T", v))
+
+	}
 }
