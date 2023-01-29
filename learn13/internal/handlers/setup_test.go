afc48d5450b543fe93a76813d26dcd0d30c92798
Liberbell <liberbell@gmail.com>
2023-01-29 13:06:59 +0900

    setup_test2

---
+++ learn13/internal/handlers/setup_test.go
@@ -0,0 +1,33 @@
+package handlers
+
+import (
+	"github.com/alexedwards/scs/v2"
+	"net/http"
+)
+
+func getRoutes() http.Handler {
+	gob.Register(models.Reservation{})
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		// return err
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+	return nil
+}
