0d6b2bbbce96f7235f07480f11406636a9499b77
Liberbell <liberbell@gmail.com>
2023-01-30 12:28:34 +0900

    handlers_test7

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -25,7 +25,13 @@ func TestHandlers(t *testing.T)  {
 
 	for _, e := range theTests {
 		if e.method == "GET" {
-			ts.Client().Get(ts.URL + e.url)
+			resp, err := ts.Client().Get(ts.URL + e.url)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+		} else {
+
 		}
 	}
 }
