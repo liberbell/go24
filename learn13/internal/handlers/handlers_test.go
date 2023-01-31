db7ddafce5d5819c49af5376b02b40f6d054e516
Liberbell <liberbell@gmail.com>
2023-02-01 08:40:12 +0900

    handlers_test4

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -60,10 +60,10 @@ func TestHandlers(t *testing.T) {
 
 		} else {
 			values := url.Values{}
-			for _, x := range.e.params{
+			for _, x := range e.params {
 				values.Add(x.key, x.value)
 			}
-			resp, err := ts.Client().PostForm(ts.URL + e.url, values)
+			resp, err := ts.Client().PostForm(ts.URL+e.url, values)
 			if err != nil {
 				t.Log(err)
 				t.Fatal(err)
