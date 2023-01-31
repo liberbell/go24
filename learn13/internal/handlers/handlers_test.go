5f23ae070f1c04919a01a4461c6d000a94059a21
Liberbell <liberbell@gmail.com>
2023-01-31 09:01:37 +0900

    handlers_test11

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -34,8 +34,10 @@ var theTests = []struct {
 		{key: "end", value: "2023-01-31"},
 	}, http.StatusOK},
 	{"makereservation", "/make-reservation", "POST", []postData{
-		{key: "start", value: "2023-01-31"},
-		{key: "end", value: "2023-01-31"},
+		{key: "first_name", value: "eric"},
+		{key: "last_name", value: "clapton"},
+		{key: "email", value: "a@b.com"},
+		{key: "phone", value: "123-456-7890"},
 	}, http.StatusOK},
 }
 
