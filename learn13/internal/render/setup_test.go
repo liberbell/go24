c43006a5094a8b1062bb15528eaceb84b6bd3e09
Liberbell <liberbell@gmail.com>
2023-02-01 08:55:26 +0900

    setup_test3

--- learn13/internal/render/setup_test.go
+++ learn13/internal/render/setup_test.go
@@ -1,5 +1,9 @@
 package render
 
-import "github.com/alexedwards/scs/v2"
+import (
+	"github.com/alexedwards/scs/v2"
+	"github.com/tsawler/bookings/internal/config"
+)
 
 var session *scs.SessionManager
+var testApp config.AppConfig
