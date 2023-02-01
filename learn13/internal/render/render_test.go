b84c1233882e1ac2e26e6f454638020a633307de
Liberbell <liberbell@gmail.com>
2023-02-02 08:45:27 +0900

    render_test9

--- learn13/internal/render/render_test.go
+++ learn13/internal/render/render_test.go
@@ -24,6 +24,7 @@ func getSession() (*http.Request, error) {
 	r, err := http.NewRequest("GET", "/some-url", nil)
 	if err != nil {
 		return nil, err
-
 	}
+	ctx := r.Context()
+	ctx, _ = session.Load(ctx, r.Header.Get("X-Session-"))
 }
