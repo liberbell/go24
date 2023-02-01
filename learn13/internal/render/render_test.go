e277d34c1daf041f306397ab3be5be5594518dca
Liberbell <liberbell@gmail.com>
2023-02-02 08:30:25 +0900

    render_test2

--- learn13/internal/render/render_test.go
+++ learn13/internal/render/render_test.go
@@ -2,8 +2,12 @@ package render
 
 import (
 	"github.com/tsawler/bookings/internal/models"
+	"net/http"
+	"testing"
 )
 
 func TestAddDefaultData(t *testing.T) {
 	var td models.TemplateData
+
+	r, err := http.NewRequest("GET", "/some-url", nil)
 }
