7f138c6fef88fb1e3ed433254a72e365623eca2f
Liberbell <liberbell@gmail.com>
2023-02-02 08:34:10 +0900

    render_test3

--- learn13/internal/render/render_test.go
+++ learn13/internal/render/render_test.go
@@ -10,4 +10,12 @@ func TestAddDefaultData(t *testing.T) {
 	var td models.TemplateData
 
 	r, err := http.NewRequest("GET", "/some-url", nil)
+	if err != nil {
+		t.Error(error)
+	}
+	result := AddDefaultData(td, r)
+
+	if result == nil {
+		t.Error("failed to add default data.")
+	}
 }
