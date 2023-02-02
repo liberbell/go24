d13fd0fab79cfcd1ecff25a69a7b322cb5356514
Liberbell <liberbell@gmail.com>
2023-02-03 08:38:39 +0900

    render3

--- learn13/internal/render/render.go
+++ learn13/internal/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -43,6 +44,7 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, td *mod
 	t, ok := tc[tmpl]
 	if !ok {
 		log.Fatal("could not get template from template cache")
+		return errors.New("can`t get template from chache")
 	}
 	buf := new(bytes.Buffer)
 	td = AddDefaultData(td, r)
