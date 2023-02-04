06593800d7404e595ee621adcce433aa36ecc5c3
Liberbell <liberbell@gmail.com>
2023-02-04 10:42:53 +0900

    forms_test2

--- learn13/internal/forms/forms_test.go
+++ learn13/internal/forms/forms_test.go
@@ -7,4 +7,10 @@ import (
 
 func TestForm_Valid(t *testing.T) {
 	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	is_valid := form.Valid()
+	if !is_valid {
+		t.Error("got invalid when shoud have benn valid")
+	}
 }
