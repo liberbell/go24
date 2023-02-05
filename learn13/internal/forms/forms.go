6944f33b4cc1782a9feaaaf09b4788156c54c007
Liberbell <liberbell@gmail.com>
2023-02-06 08:40:21 +0900

    forms1

--- learn13/internal/forms/forms.go
+++ learn13/internal/forms/forms.go
@@ -34,7 +34,7 @@ func (f *Form) Required(field ...string) {
 	}
 }
 
-func (f *Form) Has(field string, r *http.Request) bool {
+func (f *Form) Has(field string) bool {
 	x := f.Get(field)
 	if x == "" {
 		return false
