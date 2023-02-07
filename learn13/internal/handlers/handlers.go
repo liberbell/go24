875daceaed6dd6a8bbf4ef72b4bfb9e15534f666
Liberbell <liberbell@gmail.com>
2023-02-08 08:43:30 +0900

    handlers1

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -28,8 +28,8 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
-	remoteIP := r.RemoteAddr
-	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+	// remoteIP := r.RemoteAddr
+	// m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
 	render.RenderTemplate(w, r, "home.page.tmpl", &models.TemplateData{})
 }
