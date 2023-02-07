f5ffd32a0673c7b182a9a05f593a8892bf3a2197
Liberbell <liberbell@gmail.com>
2023-02-08 08:35:52 +0900

    helpers1

--- learn13/internal/helpers/helpers.go
+++ learn13/internal/helpers/helpers.go
@@ -1,9 +1,19 @@
 package helpers
 
-import "github.com/tsawler/bookings/internal/config"
+import (
+	"net/http"
+
+	"github.com/tsawler/bookings/internal/config"
+)
 
 var app *config.AppConfig
 
 func NewHelpers(a *config.AppConfig) {
 	app = a
 }
+
+func ClientError(w http.ResponseWriter, status int) {
+
+}
+
+func ServerError(w http.ResponseWriter, err error)
