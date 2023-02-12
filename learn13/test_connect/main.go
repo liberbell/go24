d07f575ba412ac5ab5f726182d7533a1f8f60949
Liberbell <liberbell@gmail.com>
2023-02-13 08:38:37 +0900

    main6

--- learn13/test_connect/main.go
+++ learn13/test_connect/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	DatabaseURL := "postgres://dbmaster:@localhost:5432/test_connect"
 	// _, err := pgx.Connect("pgx", "host=localhost port=5432 dbname=test_coneect user=dbmaster password")
-	_, err := pgx.Connect(context.Background(), os.Getenv("DatabaseURL"))
+	_, err := pgx.Connect(context.Background(), os.Getenv(DatabaseURL))
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Unable to conncet: %v\n", err))
 	}
