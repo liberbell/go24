cda6a1a7dacb8b7f1fbe652c63b20d617db26cda
Liberbell <liberbell@gmail.com>
2023-02-13 08:33:09 +0900

    main2

--- learn13/test_connect/main.go
+++ learn13/test_connect/main.go
@@ -3,5 +3,5 @@ package main
 import "database/sql"
 
 func main() {
-	conn, err := sql.Open("pgx", "host=localhost port=5432")
+	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_coneect user=dbmaster password")
 }
