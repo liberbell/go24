9b40f2b7c7f9fa7767227e990b0bba5279f14a10
Liberbell <liberbell@gmail.com>
2023-02-13 08:34:07 +0900

    main4

--- learn13/test_connect/main.go
+++ learn13/test_connect/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
 
 func main() {
 	conn, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_coneect user=dbmaster password")
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Unable to conncet: %v\n", err))
+	}
 }
