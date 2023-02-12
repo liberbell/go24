632b7afea4a5662bc8f85d55584503e553ffd0b5
Liberbell <liberbell@gmail.com>
2023-02-13 08:42:28 +0900

    main6

--- learn13/test_connect/main.go
+++ learn13/test_connect/main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"context"
+	"database/sql"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/jackc/pgx/v5"
+	// "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
-	DatabaseURL := "postgres://dbmaster:@localhost:5432/test_connect"
-	// _, err := pgx.Connect("pgx", "host=localhost port=5432 dbname=test_coneect user=dbmaster password")
-	_, err := pgx.Connect(context.Background(), os.Getenv(DatabaseURL))
+	// DatabaseURL := "postgres://dbmaster:@localhost:5432/test_connect"
+	_, err := sql.Open("pgx", "host=localhost port=5432 dbname=test_coneect user=dbmaster password")
+	// _, err := pgx.Connect(context.Background(), os.Getenv(DatabaseURL))
 	if err != nil {
 		log.Fatal(fmt.Sprintf("Unable to conncet: %v\n", err))
 	}
