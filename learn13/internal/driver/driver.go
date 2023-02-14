a7cc4ad43474e2fcb82818ab8af5fe1e6d09e534
Liberbell <liberbell@gmail.com>
2023-02-15 08:47:18 +0900

    driver3

--- learn13/internal/driver/driver.go
+++ learn13/internal/driver/driver.go
@@ -14,3 +14,11 @@ var dbConn = &DB{}
 const maxOpenDBConn = 10
 const maxIdleDBConn = 5
 const maxDBLifetime = 5 * time.Minute
+
+func ConnectSQL(dsn string) (*DB, error) {
+
+}
+
+func NewDatabase(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("dbx", dsn)
+}
