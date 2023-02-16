de3dbc8ec8e359e89ab603401a39b44fc1427686
Liberbell <liberbell@gmail.com>
2023-02-17 08:34:31 +0900

    handlers3

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -152,7 +152,7 @@ func (m *Repository) ReservationSummary(w http.ResponseWriter, r *http.Request) 
 	data := make(map[string]interface{})
 	data["reservation"] = reservation
 
-	render.RenderTemplate(w, r, "reservation-summay.page.tmpl", &models.TemplateData{
+	render.RenderTemplate(w, r, "reservation-summary.page.tmpl", &models.TemplateData{
 		Data: data,
 	})
 }
