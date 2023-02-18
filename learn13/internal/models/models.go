f85e9ac5eb1089da75d631baa7f041c3dbf9c29c
Liberbell <liberbell@gmail.com>
2023-02-18 09:59:07 +0900

    models7

--- learn13/internal/models/models.go
+++ learn13/internal/models/models.go
@@ -2,13 +2,6 @@ package models
 
 import "time"
 
-type Reservation struct {
-	FirstName string
-	LastName  string
-	Email     string
-	Phone     string
-}
-
 type Users struct {
 	ID          int
 	FirstName   string
