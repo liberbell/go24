36a7b662d4f7739a0e19ac7653b963390aa02d68
Liberbell <liberbell@gmail.com>
2023-02-19 23:04:31 +0900

    postgres6

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -11,10 +11,12 @@ func (m *postgresDBRepo) AllUsers() bool {
 }
 
 func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
 	stmt := `INSERT INTO reservation (first_name, last_name, emain, phone, start_date, end_data, room_id, created_at, updated_at)
 	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	_, err := m.DB.Exec(stmt,
+	_, err := m.DB.ExecContext(stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
