770ae6d451301fdde6f9189ace8bfc2ec07022f0
Liberbell <liberbell@gmail.com>
2023-02-19 23:01:59 +0900

    postgres5

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -13,7 +13,7 @@ func (m *postgresDBRepo) AllUsers() bool {
 func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	stmt := `INSERT INTO reservation (first_name, last_name, emain, phone, start_date, end_data, room_id, created_at, updated_at)
 	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	m.DB.Exec(stmt,
+	_, err := m.DB.Exec(stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
@@ -23,6 +23,9 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 		res.RoomID,
 		time.Now(),
 		time.Now(),
-	)
+		)
+	if err =! nil {
+		return err
+	}
 	return nil
 }
