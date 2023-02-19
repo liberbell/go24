05b116111525c11a819535a6e8f8b01757473a6b
Liberbell <liberbell@gmail.com>
2023-02-19 23:05:27 +0900

    postgres7

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -16,7 +16,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 
 	stmt := `INSERT INTO reservation (first_name, last_name, emain, phone, start_date, end_data, room_id, created_at, updated_at)
 	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
-	_, err := m.DB.ExecContext(stmt,
+	_, err := m.DB.ExecContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
 		res.Email,
