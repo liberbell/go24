e03789307d4904c73317f23782962d971173d25b
Liberbell <liberbell@gmail.com>
2023-02-19 23:00:21 +0900

    postgres4

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -1,6 +1,10 @@
 package dbrepo
 
-import "github.com/tsawler/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/tsawler/bookings/internal/models"
+)
 
 func (m *postgresDBRepo) AllUsers() bool {
 	return true
@@ -9,5 +13,16 @@ func (m *postgresDBRepo) AllUsers() bool {
 func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	stmt := `INSERT INTO reservation (first_name, last_name, emain, phone, start_date, end_data, room_id, created_at, updated_at)
 	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+	m.DB.Exec(stmt,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		time.Now(),
+		time.Now(),
+	)
 	return nil
 }
