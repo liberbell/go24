9a4e57a82b28ff850ac77111bf384bac077e3db9
Liberbell <liberbell@gmail.com>
2023-02-19 22:54:40 +0900

    postgres1

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -1,5 +1,11 @@
 package dbrepo
 
+import "github.com/tsawler/bookings/internal/models"
+
 func (m *postgresDBRepo) AllUsers() bool {
 	return true
+}
+
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+	return nil
 }
