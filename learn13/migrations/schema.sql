6c4d4f34277d740fd4d9a7ae4485197ec4a85ec9
Liberbell <liberbell@gmail.com>
2023-02-21 09:15:42 +0900

    schema2

--- learn13/migrations/schema.sql
+++ learn13/migrations/schema.sql
@@ -316,13 +316,6 @@ CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USIN
 
 
 --
--- Name: users_email_idx; Type: INDEX; Schema: public; Owner: hideakiehara
---
-
-CREATE UNIQUE INDEX users_email_idx ON public.users USING btree (email);
-
-
---
 -- Name: reservations reservations_rooms_id_fk; Type: FK CONSTRAINT; Schema: public; Owner: hideakiehara
 --
 
