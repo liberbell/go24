ef5cfd5de1a268ab883b65db5a78867a36a2b97a
Liberbell <liberbell@gmail.com>
2023-02-22 12:21:08 +0900

    postgres5

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -16,7 +16,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	defer cancel()
 
 	stmt := `INSERT INTO reservations (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at)
-	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 	_, err := m.DB.ExecContext(ctx, stmt,
 		res.FirstName,
 		res.LastName,
