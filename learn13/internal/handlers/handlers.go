b4d173e065db652e42e4a437c9abc9e6beb63d8b
Liberbell <liberbell@gmail.com>
2023-02-23 14:04:49 +0900

    handlers3

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -123,6 +123,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		RestrictionID: 1,
 	}
 
+	err = m.DB.InsertRoomRestriction(restriction)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
