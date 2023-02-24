494ff1a942cbbcb9d1c6438b7e529725d1d9d783
Liberbell <liberbell@gmail.com>
2023-02-24 23:50:52 +0900

    repository1

--- learn13/internal/repository/repository.go
+++ learn13/internal/repository/repository.go
@@ -1,10 +1,15 @@
 package repository
 
-import "github.com/tsawler/bookings/internal/models"
+import (
+	"time"
+
+	"github.com/tsawler/bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
