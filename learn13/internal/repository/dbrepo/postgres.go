83fbb5c23225dc2de542d6386dcf62257b767d9c
Liberbell <liberbell@gmail.com>
2023-02-24 23:53:18 +0900

    postgres9

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -60,13 +60,15 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	return nil
 }
 
-func (m *postgresDBRepo) SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error) {
+func (m *postgresDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	var numRows int
 
-	query := `SELECT count(id) FROM room_restrictions WHERE room_id = $1 AND $2 < end_date AND $3 > start_date;`
+	query := `SELECT count(id) 
+			  FROM room_restrictions 
+			  WHERE room_id = $1 AND $2 < end_date AND $3 > start_date;`
 
 	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
 	err := row.Scan(&numRows)
