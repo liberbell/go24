6abc036e0d5915885e3866f6a8645eb3f3f6d670
Liberbell <liberbell@gmail.com>
2023-02-26 16:48:32 +0900

    handlers6

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -176,6 +176,11 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 		m.App.InfoLog.Println("Room:", i.ID, i.RoomName)
 	}
 
+	if len(rooms) == 0 {
+		//no availability
+		m.App.InfoLog("No availability")
+	}
+
 	w.Write([]byte(fmt.Sprintf("start date is %s and end date is %s", startDate, endDate)))
 }
 
