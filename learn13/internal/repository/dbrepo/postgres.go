34ea3e0a900f8304fb9c41c1adca3e7ad48cc4cb
Liberbell <liberbell@gmail.com>
2023-02-26 16:37:56 +0900

    postgres6

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -93,7 +93,7 @@ func (m *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]
 		SELECT r.id, r.room_name 
 		FROM rooms r 
 		WHERE r.id not in 
-			(SELECT rr.room_id FROM room_restrictions rr WHERE '2023-01-13' < rr.end_date AND '2023-01-09' > rr.start_date)
+			(SELECT room_id FROM room_restrictions rr WHERE $1 < rr.end_date AND $2 > rr.start_date)
 		`
 	rows, err := m.DB.QueryContext(ctx, query, start, end)
 	if err != nil {
