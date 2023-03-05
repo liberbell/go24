73c489af6913a35daeba3164e065bc6e31e8ce6b
Liberbell <liberbell@gmail.com>
2023-03-05 19:22:46 +0900

    routes1

--- learn13/cmd/web/routes.go
+++ learn13/cmd/web/routes.go
@@ -23,7 +23,7 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
-	mux.Get("/general-quarters", handlers.Repo.Generals)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
 	mux.Get("/majors-suite", handlers.Repo.Majors)
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
