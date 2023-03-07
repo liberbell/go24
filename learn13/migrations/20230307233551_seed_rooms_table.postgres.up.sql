a08723bbfa4c324d11d25a4ffe0a4bd9f72bb652
Liberbell <liberbell@gmail.com>
2023-03-08 08:37:12 +0900

    seedroomstable1

---
+++ learn13/migrations/20230307233551_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+	 ('General Quarters ','2023-01-01 00:00:00.000','2023-01-01 00:00:00.000'),
+	 ('Major suite','2023-01-01 00:00:00.000','2023-01-01 00:00:00.000');
