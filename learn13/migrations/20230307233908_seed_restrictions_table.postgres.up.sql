28dbf354d0f57f5035c833f3f2cfda2de7dd1e4f
Liberbell <liberbell@gmail.com>
2023-03-08 08:41:26 +0900

    seedrestrictionstableup1

---
+++ learn13/migrations/20230307233908_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2023-01-11 00:00:00.000','2023-01-12 00:00:00.000'),
+	 ('Owner block','2023-01-13 00:00:00.000','2023-01-14 00:00:00.000');
