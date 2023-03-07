1b42e326c9f3e3c8a8d8e4dcffb09fe63787c60a
Liberbell <liberbell@gmail.com>
2023-03-08 08:58:40 +0900

    testrepo2

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -12,30 +12,7 @@ func (m *testDBRepo) AllUsers() bool {
 }
 
 func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	var newID int
-
-	stmt := `INSERT INTO reservations (first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at)
-	         values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
-	err := m.DB.QueryRowContext(ctx, stmt,
-		res.FirstName,
-		res.LastName,
-		res.Email,
-		res.Phone,
-		res.StartDate,
-		res.EndDate,
-		res.RoomID,
-		time.Now(),
-		time.Now(),
-	).Scan(&newID)
-
-	if err != nil {
-		return 0, err
-	}
-
-	return newID, nil
+	return 1, nil
 }
 
 func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
