978de3577720fa087912d0d584bc8d7e4f373354
Liberbell <liberbell@gmail.com>
2023-03-08 09:01:44 +0900

    testrepo3

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -24,38 +24,7 @@ func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roo
 }
 
 func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]models.Room, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
 	var rooms []models.Room
-
-	query := `
-		SELECT r.id, r.room_name 
-		FROM rooms r 
-		WHERE r.id not in 
-			(SELECT room_id FROM room_restrictions rr WHERE $1 < rr.end_date AND $2 > rr.start_date)
-		`
-	rows, err := m.DB.QueryContext(ctx, query, start, end)
-	if err != nil {
-		return rooms, err
-	}
-
-	for rows.Next() {
-		var room models.Room
-		err := rows.Scan(
-			&room.ID,
-			&room.RoomName,
-		)
-		if err != nil {
-			return rooms, err
-		}
-
-		rooms = append(rooms, room)
-	}
-	if err = rows.Err(); err != nil {
-		return rooms, err
-	}
-
 	return rooms, nil
 }
 
