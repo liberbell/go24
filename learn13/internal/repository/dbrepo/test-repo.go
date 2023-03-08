ca8242ad8df31b582a1d37e462723846dd26fa18
Liberbell <liberbell@gmail.com>
2023-03-08 09:02:30 +0900

    testrepo4

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -1,7 +1,6 @@
 package dbrepo
 
 import (
-	"context"
 	"time"
 
 	"github.com/tsawler/bookings/internal/models"
@@ -29,22 +28,6 @@ func (m *testDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]mode
 }
 
 func (m *testDBRepo) GetRoomByID(id int) (models.Room, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
 	var room models.Room
-	query := `SELECT id, room_name, created_at, updated_at FROM rooms WHERE id = $1`
-
-	row := m.DB.QueryRowContext(ctx, query, id)
-	err := row.Scan(
-		&room.ID,
-		&room.RoomName,
-		&room.CreatedAt,
-		&room.UpdatedAt,
-	)
-
-	if err != nil {
-		return room, err
-	}
 	return room, nil
 }
