37205d923a15afbfaaa32349fe5482468e9fb686
Liberbell <liberbell@gmail.com>
2023-03-09 08:55:11 +0900

    handlers_test6

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -90,6 +90,12 @@ func TestRepository_Reservation(t *testing.T) {
 
 	rr := httptest.NewRecorder()
 	session.Put(ctx, "reservation", reservation)
+
+	handler := http.HandlerFunc(Repo.Reservation)
+	handler.ServeHTTP(rr, req)
+	if rr.Code != http.StatusOK {
+		t.Errorf("Reservation handler returned wrong response code got %d, wanted %d", rr.Code, http.StatusOK)
+	}
 }
 
 func getCtx(req *http.Request) context.Context {
