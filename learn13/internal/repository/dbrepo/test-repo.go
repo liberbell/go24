006a2d95087fe656ae69f5ccf5d31660e3dad147
Liberbell <liberbell@gmail.com>
2023-03-08 09:00:46 +0900

    testrepo2

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -16,47 +16,10 @@ func (m *testDBRepo) InsertReservation(res models.Reservation) (int, error) {
 }
 
 func (m *testDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	stmt := `INSERT INTO room_restrictions (start_date, end_date, room_id, reservation_id, created_at, updated_at, restriction_id)
-		values ($1, $2, $3, $4, $5, $6, $7)`
-
-	_, err := m.DB.ExecContext(ctx, stmt,
-		r.StartDate,
-		r.EndDate,
-		r.RoomID,
-		r.ReservationID,
-		time.Now(),
-		time.Now(),
-		r.RestrictionID,
-	)
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
 func (m *testDBRepo) SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-
-	var numRows int
-
-	query := `SELECT count(id) 
-			  FROM room_restrictions 
-			  WHERE room_id = $1 AND $2 < end_date AND $3 > start_date;`
-
-	row := m.DB.QueryRowContext(ctx, query, roomID, start, end)
-	err := row.Scan(&numRows)
-	if err != nil {
-		return false, err
-	}
-
-	if numRows == 0 {
-		return true, nil
-	}
-
 	return false, nil
 }
 
