a8bd5e7ff182bca334c4555a1ec45a773d974e11
Liberbell <liberbell@gmail.com>
2023-03-10 08:40:13 +0900

    handlers1

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -59,7 +59,8 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 	// var emptyReservation models.Reservation
 	res, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
-		helpers.ServerError(w, errors.New("Cannot get reservation from session"))
+		m.App.Session.Put(r.Context(), "error", "can't get reservation from session")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
