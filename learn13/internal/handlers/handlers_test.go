11e6c22cca00526007007ba0edc01aa85f9067a9
Liberbell <liberbell@gmail.com>
2023-03-10 09:02:42 +0900

    handlers_test7

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -88,18 +88,18 @@ func TestRepository_Reservation(t *testing.T) {
 			RoomName: "General Quarters",
 		},
 	}
-	req, _ := http.NewRequest("GET", "/make-reservation", nil)
-	ctx := getCtx(req)
-	req = req.WithContext(ctx)
+	// req, _ := http.NewRequest("GET", "/make-reservation", nil)
+	// ctx := getCtx(req)
+	// req = req.WithContext(ctx)
 
-	rr := httptest.NewRecorder()
-	session.Put(ctx, "reservation", reservation)
+	// rr := httptest.NewRecorder()
+	// session.Put(ctx, "reservation", reservation)
 
-	handler := http.HandlerFunc(Repo.Reservation)
-	handler.ServeHTTP(rr, req)
-	if rr.Code != http.StatusOK {
-		t.Errorf("Reservation handler returned wrong response code got %d, wanted %d", rr.Code, http.StatusOK)
-	}
+	// handler := http.HandlerFunc(Repo.Reservation)
+	// handler.ServeHTTP(rr, req)
+	// if rr.Code != http.StatusOK {
+	// 	t.Errorf("Reservation handler returned wrong response code got %d, wanted %d", rr.Code, http.StatusOK)
+	// }
 
 	req, _ := http.NewRequest("GET", "/make-reservation", nil)
 	ctx = getCtx(req)
