f5ab330b3d9c9b5c20a90d7f5efa1f97cb0263b3
Liberbell <liberbell@gmail.com>
2023-03-10 09:00:53 +0900

    handlers_test6

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -97,7 +97,7 @@ func TestRepository_Reservation(t *testing.T) {
 
 	handler := http.HandlerFunc(Repo.Reservation)
 	handler.ServeHTTP(rr, req)
-	if rr.Code == http.StatusOK {
+	if rr.Code != http.StatusOK {
 		t.Errorf("Reservation handler returned wrong response code got %d, wanted %d", rr.Code, http.StatusOK)
 	}
 
