613d988b2576a8c298c217c1d57b720ea0b04201
Liberbell <liberbell@gmail.com>
2023-03-11 11:06:21 +0900

    handlers6

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -90,7 +90,7 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
-	reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
+	// reservation, ok := m.App.Session.Get(r.Context(), "reservation").(models.Reservation)
 	if !ok {
 		m.App.Session.Put(r.Context(), "error", "can't parse form")
 		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
