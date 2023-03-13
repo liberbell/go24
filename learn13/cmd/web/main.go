d7416baeed6fcbd778c92c804b55b27481e68416
Liberbell <liberbell@gmail.com>
2023-03-14 08:57:02 +0900

    main2

--- learn13/cmd/web/main.go
+++ learn13/cmd/web/main.go
@@ -35,7 +35,7 @@ func main() {
 
 	from := "some@example.com"
 	auth := smtp.PlainAuth("user", from, "password", "localhost")
-	err = smtp.SendMail("localhost:1025", auth, from, []string{"you@example.com"})
+	err = smtp.SendMail("localhost:1025", auth, from, []string{"you@example.com"}, []byte("Hello world"))
 
 	// gob.Register(models.Reservation{})
 	// gob.Register(models.User{})
