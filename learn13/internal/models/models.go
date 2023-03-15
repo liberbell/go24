a3c06631a8caba3deee92a53f0f705013db8e35e
Liberbell <liberbell@gmail.com>
2023-03-16 08:45:11 +0900

    models1

--- learn13/internal/models/models.go
+++ learn13/internal/models/models.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"html/template"
 	"time"
 )
 
@@ -62,5 +61,5 @@ type MailData struct {
 	To      string
 	From    string
 	Subject string
-	Content template.HTML
+	Content string
 }
