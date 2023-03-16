17a77db56778a7ca0d1f4ff15a518ac0aff883ed
Liberbell <liberbell@gmail.com>
2023-03-17 08:50:27 +0900

    handlers5

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -190,7 +190,7 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		<strong>Reservation Confirmation</strong><br>
 		Dear %s: ,<br>
 		this is confirm your reservation from %s to %s.
-		`, reservation.FirstName, reservation.FirstName.Format("2006-01-01"), reservation.EndDate.Format("2006-01-01"))
+		`, reservation.FirstName, reservation.StartDate.Format("2006-01-01"), reservation.EndDate.Format("2006-01-01"))
 
 	msg := models.MailData{
 		To:      reservation.Email,
