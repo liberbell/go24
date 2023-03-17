cdff987e69c1fb08be5bff2fe8c5619ca67309f3
Liberbell <liberbell@gmail.com>
2023-03-17 09:02:53 +0900

    handlers6

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -201,6 +201,18 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 
 	m.App.MailChan <- msg
 
+	htmlMessage = fmt.Sprintf(`
+		<strong>Reservation Notification</strong><br>
+		A reservation has been made for %s from %s to %s.
+		`, reservation.FirstName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
+
+	msg = models.MailData{
+		To:      reservation.Email,
+		From:    "nobady@example.com",
+		Subject: "Reservation confirmation",
+		Content: htmlMessage,
+	}
+
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
