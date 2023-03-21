078f74230c276ca4536819c88b8bee82372ff69c
Liberbell <liberbell@gmail.com>
2023-03-21 22:09:20 +0900

    testrepo1

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -48,3 +48,11 @@ func (m *testDBRepo) GetUserByID(id int) (models.User, error) {
 
 	return u, nil
 }
+
+func (m *testDBRepo) UpdateUser(u models.User) error {
+	return nil
+}
+
+func (m *testDBRepo) Authentiate(email, testPassword, string) (int, string, error) {
+	return 1, "", nil
+}
