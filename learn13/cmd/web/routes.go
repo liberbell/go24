42eb8ab622215ba36cba4f5c9310d91b9677d85c
Liberbell <liberbell@gmail.com>
2023-03-22 09:56:07 +0900

    routes1

--- learn13/cmd/web/routes.go
+++ learn13/cmd/web/routes.go
@@ -38,6 +38,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/contact", handlers.Repo.Contact)
 
 	mux.Get("/user/login", handlers.Repo.ShowLogin)
+	mux.Post("/user/login", handlers.Repo.PostShowLogin)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
