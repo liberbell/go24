1d9322fdbf4cf5a0a708bc086dc9333f221bfacb
Liberbell <liberbell@gmail.com>
2023-03-24 08:52:05 +0900

    handlers3

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -461,3 +461,10 @@ func (m *Repository) PostShowLogin(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "flash", "Logged in successfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+	_ = m.App.Session.RenewToken(r.Context())
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
