b87ab81153f1d21880184c46ef373dd1afda7480
Liberbell <liberbell@gmail.com>
2023-03-28 08:52:38 +0900

    repository1

--- learn13/internal/repository/repository.go
+++ learn13/internal/repository/repository.go
@@ -16,4 +16,5 @@ type DatabaseRepo interface {
 	GetRoomByID(id int) (models.Room, error)
 	GetUserByID(id int) (models.User, error)
 	Authenticate(email, testPassword string) (int, string, error)
+	AllReservations() ([]models.Reservation, error)
 }
