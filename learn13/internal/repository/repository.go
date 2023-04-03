53220e120f6d5f7e6b9845436de8092bfbc183bf
Liberbell <liberbell@gmail.com>
2023-04-03 09:57:18 +0900

    repository1

--- learn13/internal/repository/repository.go
+++ learn13/internal/repository/repository.go
@@ -20,4 +20,5 @@ type DatabaseRepo interface {
 	AllNewReservations() ([]models.Reservation, error)
 	GetReservationByID(id int) (models.Reservation, error)
 	UpdateReservation(u models.Reservation) error
+	DeleteReservation(id int) error
 }
