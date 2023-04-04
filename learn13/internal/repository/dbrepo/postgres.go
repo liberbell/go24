7d3210a8a3892ba2383c325e365caff1075c054c
Liberbell <liberbell@gmail.com>
2023-04-04 13:13:57 +0900

    postgres1

--- learn13/internal/repository/dbrepo/postgres.go
+++ learn13/internal/repository/dbrepo/postgres.go
@@ -332,13 +332,15 @@ func (m *postgresDBRepo) UpdateReservation(u models.Reservation) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := `UPDATE reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5`
+	query := `UPDATE reservations set first_name = $1, last_name = $2, email = $3, phone = $4, updated_at = $5
+			  WHERE id = $6 `
 	_, err := m.DB.ExecContext(ctx, query,
 		u.FirstName,
 		u.LastName,
 		u.Email,
 		u.Phone,
 		time.Now(),
+		u.ID
 	)
 	if err != nil {
 		return err
