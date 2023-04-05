b4cea2670e1d20564a6aae30d91be2efe72127c7
Liberbell <liberbell@gmail.com>
2023-04-06 08:48:00 +0900

    handlers2

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -577,7 +577,7 @@ func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Requ
 func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Request) {
 	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
 	src := chi.URLParam(r, "src")
-	_ = m.DB.DeleteReservation(id, 1)
+	_ = m.DB.DeleteReservation(id)
 	m.App.Session.Put(r.Context(), "flash", "Reservation deleted.")
 	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
