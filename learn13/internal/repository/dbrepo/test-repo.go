d0fd98af4b548e8b1e40bbae0bb12c983e83cd79
Liberbell <liberbell@gmail.com>
2023-04-12 08:33:30 +0900

    testrepo1

--- learn13/internal/repository/dbrepo/test-repo.go
+++ learn13/internal/repository/dbrepo/test-repo.go
@@ -92,3 +92,9 @@ func (m *testDBRepo) AllRooms() ([]models.Room, error) {
 
 	return rooms, nil
 }
+
+func (m *testDBRepo) GetRestrictionsForRoomByDate(roomID int, start, end, time.Time) ([]models.RoomRestriction, error) {
+	var restrictions []models.Restriction
+
+	return restrictions, nil
+}
