f1d66d91e3e0e4cd68cff017a85774a351d65608
Liberbell <liberbell@gmail.com>
2023-04-12 08:48:42 +0900

    handlers4

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -618,7 +618,9 @@ func (m *Repository) AdminReservationsCalender(w http.ResponseWriter, r *http.Re
 		}
 		for _, y := range restrictions {
 			if y.ReservationID > 0 {
-				a
+				for d := y.StartDate; d.After(y.EndDate) == false; d = d.AddDate(0, 0, 1) {
+					reservationMap[d.Format("2006 01 2")] = y.ReservationID
+				}
 			} else {
 
 			}
