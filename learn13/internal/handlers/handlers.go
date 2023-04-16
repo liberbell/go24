9b619d6fc864d00179179fd352d3e5c0fa97640c
Liberbell <liberbell@gmail.com>
2023-04-16 13:14:04 +0900

    handlers4

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -221,6 +221,13 @@ func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
 }
 
 func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		m.App.Session.Put(r.Context(), "error", "can't parse form!")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 
