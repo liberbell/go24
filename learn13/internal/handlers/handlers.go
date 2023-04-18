e153fa4c4b1fdc75eb1aec09b7798480d8ad08aa
Liberbell <liberbell@gmail.com>
2023-04-19 08:29:58 +0900

    handlers4

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -652,7 +652,7 @@ func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Requ
 
 	m.App.Session.Put(r.Context(), "flash", "Reservation marked as processed")
 	if year == "" {
-		http.Redirect(w, r, fmt.Sprintf("/admin/reservations-#{src}"), http.StatusSeeOther)
+		http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 	} else {
 		http.Redirect(w, r, fmt.Sprintf("/admin/reservations-calendar?y=%s&m=%s", year, month), http.StatusSeeOther)
 	}
