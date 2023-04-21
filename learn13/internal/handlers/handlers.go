a0a90ef28dcc19f55b749fbc79dd9aa4cc4841ac
Liberbell <liberbell@gmail.com>
2023-04-22 00:56:22 +0900

    handlers1

--- learn13/internal/handlers/handlers.go
+++ learn13/internal/handlers/handlers.go
@@ -125,6 +125,13 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	room, err := m.DB.GetRoomByID()
+	if err != nil {
+		m.App.Session.Put(r.Context(), "error", "invalid data")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
 	reservation := models.Reservation{
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
