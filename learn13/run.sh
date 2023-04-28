3d5b9295607b92eabb7e506c0bab918780c99f99
Liberbell <liberbell@gmail.com>
2023-04-28 23:22:45 +0900

    run4

--- learn13/run.sh
+++ learn13/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 go build -o bookings cmd/web/*.go
-./bookings
-# ./bookings -dbname=bookings -dbuser=dbmaser -cache=false -production=false
+# ./bookings
+./bookings -dbname=bookings -dbuser=dbmaser -cache=false -production=false
