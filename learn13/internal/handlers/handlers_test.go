a2c217e56b1cddf01e050804ace973ac52f3975b
Liberbell <liberbell@gmail.com>
2023-04-29 23:03:21 +0900

    handlers_test11

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -488,6 +488,13 @@ func TestReservationmSumary(t *testing.T) {
 		if rr.Code != e.expectedStatusCode {
 			t.Errorf("%s returned wrong response code: got %d, wanted %d", e.name, rr.Code, e.expectedStatusCode)
 		}
+
+		if e.expectedLocation != "" {
+			actualLoc, _ := rr.Result().Location()
+			if actualLoc.String() != e.expectedLocation {
+				t.Errorf("failed %s: expected location %s, but got location %s", e.name, e.expectedLocation, actualLoc.String())
+			}
+		}
 	}
 }
 
