7f91ae46979c95e7613e92b3a4c84963d3ef3ae5
Liberbell <liberbell@gmail.com>
2023-04-29 17:20:44 +0900

    handlers_test9

--- learn13/internal/handlers/handlers_test.go
+++ learn13/internal/handlers/handlers_test.go
@@ -469,7 +469,7 @@ var reservationSummaryTests = []struct {
 	},
 }
 
-func TestReservationSumary(t *testing.T) {
+func TestReservationmSumary(t *testing.T) {
 	for _, e := range reservationSummaryTests {
 
 		req, _ := http.NewRequest("GET", e.url, nil)
