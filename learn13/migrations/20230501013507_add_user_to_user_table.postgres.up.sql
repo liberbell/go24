9f0fce201adca60417ada8ffd98aebe37ac6d597
Liberbell <liberbell@gmail.com>
2023-05-01 10:40:38 +0900

    addusertousertable2

---
+++ learn13/migrations/20230501013507_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('bob','mary','bob@example.com','$2a$12$omnXkOlWMAhjvFOdUxwygutr8T3/AKEulWL2Clnq1fSTmv0fjnvHK',1,'2023-01-01 00:00:00','2023-01-01 00:00:00'),
+	 ('eric','clapton','eric@example.com','$2a$12$hioQD3Ct9ZZmDRzyaEI.IurzIM2N/D1PNmo84uC8GlsDkRZlUEVrC',3,'2023-01-01 00:00:00','2023-01-01 00:00:00');
