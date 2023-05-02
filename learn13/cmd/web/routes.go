9eb68a1153604c1f92953181f292ef7758ed4da6
Liberbell <liberbell@gmail.com>
2023-05-02 14:50:36 +0900

    routes1

--- learn13/cmd/web/routes.go
+++ learn13/cmd/web/routes.go
@@ -54,7 +54,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/process-reservation/{src}/{id}/do", handlers.Repo.AdminProcessReservation)
 		mux.Get("/delete-reservation/{src}/{id}/do", handlers.Repo.AdminDeleteReservation)
 
-		mux.Get("/reservations/{src}/{id}/show", handlers.Repo.AdminShowReservations)
+		mux.Get("/reservations/{src}/{id}/show", handlers.Repo.AdminShowReservation)
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
 	})
 
